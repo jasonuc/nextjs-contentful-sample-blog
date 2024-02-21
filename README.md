1e5737cdb244cbca3ba97ad68a1bf579ea74e10d
jasonuc <mrjasonu@gmail.com>
2024-02-21 15:38:58 +0000

    Update README and page title, add preview images

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Next.js & Contentful Sample Blog
 
+*A guide to build a blog using Contentful CMS with Next.js*
+
+### Preview of the sample blog
+![Preview of Home Page](public/Preview-Home.jpeg)
+![Preview of Blog Post](public/Preview-Blog-Post.jpeg)
+
+<hr />
+
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center space-y-20 p-10">
-      <h1 className="font-bold text-3xl">Blog Posts</h1>
+      <h1 className="font-bold text-3xl">Next.JS + Contentful Sample Blog</h1>
 
       <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-10">
 
